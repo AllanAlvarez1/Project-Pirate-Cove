19e8d05d493a0394de78ccf98f72dcc291aa378d
Allan Alvarez <88692955+AllanAlvarez1@users.noreply.github.com>
2024-05-15 23:31:28 -0400

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,24 @@
+package main;
+
+public class MainClass {
+
+	//This is the Project of Allan Alvarez for Video Game Programming Class
+	//The goal is to not just make a game but to understand how each piece connects to 
+	//each other so that it is a true learning experience.
+	//I have many thanks for YouTube videos concerning how to use color to read a spritesheet
+	//or how color can be used to make a level with the help of an image manipulator.
+	//KaarinGaming has my utmost thanks for helping me understand and explaining how a 
+	//game can be made very simply and I express that I took all of his teachings to heart.
+	//While the books did not help me much because they were confusing in some parts,
+	//Youtube taught me many ways to run animations, create a GameWindow,
+	//and how to properly code and organize a project in process. I learned about packages and
+	//where to store values and how to bring those values to other classes. With that said,
+	//this is my project, Pirate's Cove, a game where you must defeat all the enemies in the level
+	//to be able to progress to the next one.
+	
+	public static void main(String[] args) {
+	
+		new Game(); //accesses the constructor in game
+	}
+
+}
